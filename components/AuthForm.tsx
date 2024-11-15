a324679afc0fdf292ec25ebac29a9e5ef0d94423
meraj2704 <mh1669101@gmail.com>
2024-11-15 23:50:41 +0600

    sign up api hit successfully done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -35,7 +36,9 @@ const authFormSchema = (formType: FormType) => {
 };
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
+
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -46,6 +49,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email,
+      });
+
+      setAccountId(user.accountId);
+    } catch (error: any) {
+      console.log(error);
+      setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const onError = (error: any) => {
@@ -130,7 +148,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
               href={type === "sign-in" ? "/sign-up" : "/sign-in"}
               className="ml-1 font-bold text-brand"
             >
-              {type === "sign-in" ? "Sign In" : "Sign  Up"}
+              {type === "sign-in" ? "Sign Up" : "Sign  In"}
             </Link>
           </div>
         </form>
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { Query, ID } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  console.log("database id", appwriteConfig.databaseId);
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    [Query.equal("email", [email])]
+  );
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    console.log("Email token response:", session);
+    return session?.$id || null;
+  } catch (error) {
+    // console.error(error);
+    handleError(error, "Failed to send OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existUser = await getUserByEmail(email);
+  if (existUser) throw new Error("Email already exists");
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send OTP");
+  if (!existUser) {
+    const { databases } = await createAdminClient();
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToK4qEfbnd-RN82wdL2awn_PMviy_pelocqQ&s",
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify(accountId);
+};

a3a7e7c0f2c25ef241d6450eddc11a4604ec2964
meraj2704 <mh1669101@gmail.com>
2024-11-16 22:37:57 +0600

    updated otp modal code

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -16,47 +16,81 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import Image from "next/image";
 
-const OTPModal = () => {
-    const [isOpen,setIsOpen] = useState(true)
-    const [password,servers] = useState('');
-    const [isLoading,setIsLoading] = useState(false)
-    const handleSubmit= (e:React.MouseEvent<HTMLButtonElement>)=>{
-        e.preventDefault()
-        setIsLoading(true)
-        // perform deletion here
-        setIsLoading(false)
-        setIsOpen(false)
-  
+const OTPModal = ({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    // perform deletion here
+    try {
+    } catch (error: any) {
+      console.log("Failed to verify OTP.", error);
     }
+    setIsLoading(false);
+    setIsOpen(false);
+  };
+
+  const resendOTP = () => {};
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader className="relative flex justify-center">
+          <AlertDialogTitle className="h2 text-center">
+            Enter your OTP
+            <Image
+              src={"/assets/icons/close-dark.svg"}
+              alt="close"
+              height={20}
+              width={20}
+              onClick={() => setIsOpen(false)}
+              className="otp-close-btn"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription className="subtitle-2 text-center text-light-100 ">
+            We've send a code to{" "}
+            <span className="pl-1 text-brand">{email}</span>.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <InputOTP maxLength={6}>
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
+        <InputOTP maxLength={6} value={password} onChange={setPassword}>
+          <InputOTPGroup className="shad-otp">
+            <InputOTPSlot index={0} className="shad-otp-slot" />
+            <InputOTPSlot index={1} className="shad-otp-slot" />
+            <InputOTPSlot index={2} className="shad-otp-slot" />
+
+            <InputOTPSlot index={3} className="shad-otp-slot" />
+            <InputOTPSlot index={4} className="shad-otp-slot" />
+            <InputOTPSlot index={5} className="shad-otp-slot" />
           </InputOTPGroup>
         </InputOTP>
 
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <div className="flex w-full flex-col gap-4"></div>
+          <AlertDialogAction
+            onClick={handleSubmit}
+            className="shad-submit-btn h-12"
+            type="button"
+          >
+            Submit{" "}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
